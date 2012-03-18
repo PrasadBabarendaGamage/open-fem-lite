6c6e8e48d0a827aef00be29b967669c488cb19fc
Prasad Babarenda Gamage <psam012@aucklanduni.ac.nz>
2012-03-19 10:14:16 +1300

    Adding/updating license block

--- src/fem_generatedMeshRoutines.py
+++ src/fem_generatedMeshRoutines.py
@@ -1,50 +1,44 @@
-"""
-========================================================================
-FILE: fem_generatedMeshRoutines.py
+#/usr/bin/env python
 
-LAST MODIFIED: 08 June 2010
+#> \file
+#> \author Thiranja Prasad Babarenda Gamage
+#> \brief 
+#>
+#> \section LICENSE
+#>
+#> Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#>
+#> The contents of this file are subject to the Mozilla Public License
+#> Version 1.1 (the "License"); you may not use this file except in
+#> compliance with the License. You may obtain a copy of the License at
+#> http://www.mozilla.org/MPL/
+#>
+#> Software distributed under the License is distributed on an "AS IS"
+#> basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+#> License for the specific language governing rights and limitations
+#> under the License.
+#>
+#> The Original Code is Numerical Validation of elasticity problems in OpenCMISS
+#>
+#> The Initial Developer of the Original Code is Thiranja Prasad Babarenda Gamage,
+#> Auckland, New Zealand, 
+#> All Rights Reserved.
+#>
+#> Contributor(s):
+#>
+#> Alternatively, the contents of this file may be used under the terms of
+#> either the GNU General Public License Version 2 or later (the "GPL"), or
+#> the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+#> in which case the provisions of the GPL or the LGPL are applicable instead
+#> of those above. If you wish to allow use of your version of this file only
+#> under the terms of either the GPL or the LGPL, and not to allow others to
+#> use your version of this file under the terms of the MPL, indicate your
+#> decision by deleting the provisions above and replace them with the notice
+#> and other provisions required by the GPL or the LGPL. If you do not delete
+#> the provisions above, a recipient may use your version of this file under
+#> the terms of any one of the MPL, the GPL or the LGPL.
+#>
 
-DESCRIPTION: 
-Basis classes and constructor for creating and returning a basis 
-instance of the required dimensionality and basis
-========================================================================
-"""
-"""
-BEGIN LICENSE BLOCK 
-Version: MPL 1.1/GPL 2.0/LGPL 2.1
-
-The contents of this file are subject to the Mozilla Public License Version
-1.1 (the "License"); you may not use this file except in compliance with
-the License. You may obtain a copy of the License at
-http://www.mozilla.org/MPL/
-
-Software distributed under the License is distributed on an "AS IS" basis,
-WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-for the specific language governing rights and limitations under the
-License.
-
-The Original Code is fem functions.
-
-The Initial Developer of the Original Code is Thiranja Prasad Babarenda Gamage.
-Portions created by the Initial Developer are Copyright (C) 2010
-the Initial Developer. All Rights Reserved.
-
-Contributor(s):
-
-Alternatively, the contents of this file may be used under the terms of
-either the GNU General Public License Version 2 or later (the "GPL"), or
-the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
-in which case the provisions of the GPL or the LGPL are applicable instead
-of those above. If you wish to allow use of your version of this file only
-under the terms of either the GPL or the LGPL, and not to allow others to
-use your version of this file under the terms of the MPL, indicate your
-decision by deleting the provisions above and replace them with the notice
-and other provisions required by the GPL or the LGPL. If you do not delete
-the provisions above, a recipient may use your version of this file under
-the terms of any one of the MPL, the GPL or the LGPL.
-
-END LICENSE BLOCK
-"""
 
 DEBUG=False
 A = 1
